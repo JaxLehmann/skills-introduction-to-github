cff41e35baff8304013ad21d315824848f7c686a
Jax Lehmann <jaxlehmann@gmail.com>
2025-03-10 20:46:37 -0400

    Add files via upload

---
+++ NullKeyword.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class NullKeyword {
+
+	public static void main(String[] args) {
+		String a = null;
+		ArrayList b = null;
+		
+		if (b.isEmpty()) {
+			System.out.println("is null");
+		}
+		
+		// System.out.println("Cannot run methods on null objects!");
+
+	}
+
+}
